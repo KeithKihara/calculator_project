02173e447694086bc653ed3ea5b23a40e667cb95
KeithKihara <keytakingz@gmail.com>
2023-02-15 11:40:57 +0300

    added calculator

--- calculator.py
+++ calculator.py
@@ -6,14 +6,28 @@ operator = input("Enter operator:")
 
 def add(num1,num2):
     result = num1 + num2
-    return result
+    print(result)
 
 def subtact(num1,num2):
     result = num1 + num2
-    return result
+    print(result)
 
 def divide(num1,num2):
     result = num1 / num2
-    return result
+    print(result)
 
-def multiply(num1)
+def multiply(num1,num2):
+    result = num1 * num2
+    print(result)
+
+#CHECK OPERATOR
+if operator == "+":
+    add(number1,number2)
+elif operator == "-":
+    subtract(number1,number2)
+elif operator == "/":
+    divide(number1,number2)
+elif operator == "*" or operator == "X" or operator == "x":
+     multiply(number1,number2)
+else:
+    print("Invalid operator")
